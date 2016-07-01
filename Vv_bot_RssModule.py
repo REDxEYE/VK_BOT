39ff32f5037b583db314bb1b4dbb1e962a6fa21f
RED_EYE <med45c@gmail.com>
2016-07-01 20:11:33 +0300

    Обновлена система обработки сообщений

--- Vv_bot_RssModule.py
+++ Vv_bot_RssModule.py
@@ -1,8 +1,9 @@
 import datetime
+from html.parser import HTMLParser
 from time import mktime
 
 import feedparser
-from html.parser import HTMLParser
+from bs4 import BeautifulSoup as BS
 
 
 class MLStripper(HTMLParser):
@@ -31,7 +32,10 @@ def ConvertTime(date):
 python_wiki_rss_url = "http://www.oszone.net/rss-cat-2.xml"
 feed = feedparser.parse(python_wiki_rss_url)
 for I in feed['entries'][:1]:
-    print(I)
+    soup = BS(I['summary_detail']['value'], "html.parser")
+    img = soup.find_all('img')[0]['src']
+    # print(I)
     print("Date: ", ConvertTime(I.published_parsed))
     print('Text:', strip_tags(I['summary_detail']['value']))
+    print('Img: ', img)
     print('Link:', I['link'], '\n')
