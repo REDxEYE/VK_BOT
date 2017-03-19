bd46edb058437a77c5228deb494deba186ee793e
RED_EYE <med45c@gmail.com>
2017-03-20 01:34:33 +0300

    Raspberry Pi support

--- updater.py
+++ updater.py
@@ -1,8 +1,10 @@
+import os.path
 from shutil import rmtree
 from sys import exit
-
+def getpath():
+    return os.path.dirname(os.path.abspath(__file__))
 from subprocess import Popen
 
-Popen("Vk_bot2.py", shell=True) # go back to your program
+Popen('python3 '+os.path.join(getpath(),"Vk_bot2.py"), shell=True) # go back to your program
 
 exit("exit to restart the true program")
