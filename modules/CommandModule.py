c678283252b76dead1232d8ba9daf864352d37fe
RED_EYE <med45c@gmail.com>
2017-03-20 01:46:14 +0300

    Raspberry Pi support

--- modules/CommandModule.py
+++ modules/CommandModule.py
@@ -582,7 +582,8 @@ class Command_restart(C_template):
     @staticmethod
     def execute(bot, data, forward=True):
         from subprocess import Popen
-        os.execl('python3', os.path.join(bot.ROOT, __file__))
+        print(sys.executable, os.path.join(bot.ROOT, __file__))
+        os.execl(sys.executable, os.path.join(bot.ROOT, __file__))
 class _Command_restart(C_template):
     name = ["рестарт"]
     access = ['admin']
